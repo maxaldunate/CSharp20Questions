249efa12825381e850c7b1af544ff13b62603544
Max Aldunate <maxaldunate@gmail.com>
2017-05-06 09:34:40 +0200

    reorg modify base parameter

--- ConsoleAppNetCore/Program.cs
+++ ConsoleAppNetCore/Program.cs
@@ -7,27 +7,14 @@ namespace ConsoleAppNetCore
     {
         public static void Main(string[] args)
         {
-            Q01_StructureAndClasses.Run();
-            Q01_StructureAndClasses.RunByRef();
-            Q04_AssemblyLocation.Run();
-            Q12_GetIndex.Run();
-            //ModifyConstructorParameter();
-            //Q15_Enumeration.Run();
+            //Q01_StructureAndClasses.Run();
+            //Q01_StructureAndClasses.RunByRef();
+            //Q04_AssemblyLocation.Run();
+            //Q05_BaseAndInheritetClass.Run();
+            //Q12_GetIndex.Run();
+            Q15_Enumeration.Run();
 
             Console.ReadKey();
         }
-
-        public static void ModifyConstructorParameter()
-        {
-            var name = "maximiliano";
-            var baseClass = new BaseClass(name);
-            var inheritedClass = new InheritedClass(name);
-
-            Console.WriteLine($"Base class name      = '{baseClass.Name}'");
-            Console.WriteLine($"Inherited class name = '{inheritedClass.Name}'");
-        }
-
-
-
     }
 }
---
+++ ConsoleAppNetCore/Questions/Q05_BaseAndInheritetClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleAppNetCore.Questions
+{
+    public static class Q05_BaseAndInheritetClass
+    {
+        public static void Run()
+        {
+            var name = "maximiliano";
+            var baseClass = new BaseClass(name);
+            var inheritedClass = new InheritedClass(name);
+
+            Console.WriteLine($"Base class name      = '{baseClass.Name}'");
+            Console.WriteLine($"Inherited class name = '{inheritedClass.Name}'");
+        }
+
+    }
+
+    public class BaseClass
+    {
+        public string Name { get; private set; }
+
+        public BaseClass(string name)
+        {
+            Name = name;
+        }
+    }
+
+    public class InheritedClass : BaseClass
+    {
+        public InheritedClass(string name)  : base(ModifyParameter(name))
+        {
+        }
+
+        private static string ModifyParameter(string name)
+        {
+            return name.ToUpper();
+        }
+    }
+}
